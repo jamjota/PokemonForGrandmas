0ecff654e7bdb12e3fec99f78a33ef95202981a9
Jamie Tait <taitjamie@outlook.com>
2025-04-23 08:37:02 -0700

    Pokeball rendering to browser, no fetch yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-      <h1>Pokemon for Grandmas</h1>
-      <input type='text' id='pokemonName' placeholder='Enter Pokemon Name'></input>
-      <button>Fetch Pokemon</button>
-    </div>
+        <img
+          src='src/assets/Pokeball.png'
+          alt='Image of a classic Pokeball'
+          width='100'
+          height='100'
+        />
+      </div>
+      <div>
+        <h1>Pokemon for Grandmas</h1>
+        <input
+          type='text'
+          id='pokemonName'
+          placeholder='Enter Pokemon Name'
+        ></input>
+        <button>Fetch Pokemon</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 //  onClick={FetchPokemon}
