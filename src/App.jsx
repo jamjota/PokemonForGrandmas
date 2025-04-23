155ba3bc39f4274d10cc60717b2455e810d0d48c
Jamie Tait <taitjamie@outlook.com>
2025-04-23 08:13:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>Pokemon for Grandmas</h1>
+      <input type='text' id='pokemonName' placeholder='Enter Pokemon Name'></input>
+      <button>Fetch Pokemon</button>
+    </div>
+    </>
+  )
+}
+
+export default App
+//  onClick={FetchPokemon}
